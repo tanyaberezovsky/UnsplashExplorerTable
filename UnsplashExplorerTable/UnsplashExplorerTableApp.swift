70a7591efa15f93cdc7a6f5a0271270a76c70f00
tanyaberezovsky <cokacolata@gmail.com>
2024-02-07 15:01:03 +0200

    Initial Commit

---
+++ UnsplashExplorerTable/UnsplashExplorerTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashExplorerTableApp.swift
+//  UnsplashExplorerTable
+//
+//  Created by tanya on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashExplorerTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
